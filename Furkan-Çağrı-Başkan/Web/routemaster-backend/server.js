393dc584e8964c08b6c51b2f0b92a868000c94df
Fcbaskan <furkancagribaskan32@gmail.com>
2026-04-04 16:49:32 +0300

    Remove duplicate CORS middleware initialization

--- Furkan-Çağrı-Başkan/Web/routemaster-backend/server.js
+++ Furkan-Çağrı-Başkan/Web/routemaster-backend/server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-app.use(cors());
 const mongoose = require('mongoose');
 
 const { User, Travelogue, TravelogueRating, Favorite } = require('./models');
